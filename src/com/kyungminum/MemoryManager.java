df437cf11f614f76ec21a4fd097bea5947ef44eb
jangkunsuguri <jsuguri@my.bridgeport.edu>
2020-11-20 17:28:22 -0500

    no message

--- src/com/kyungminum/MemoryManager.java
+++ src/com/kyungminum/MemoryManager.java
@@ -2,15 +2,15 @@ package com.kyungminum;
 
 public class MemoryManager {
     static private final int NULL = -1;
-    private int[] memeory;
+    private int[] memory; //memory we manage
     private int top;
-    private int freestart;
+    private int freestart; //start of free list
 
     public MemoryManager(int[] initalMemory){
-        this.memeory = initalMemory;
-        top = memeory.length;
-        memeory[0] = memeory.length;
-        memeory[1] = NULL;
+        this.memory = initalMemory;
+        top = memory.length;
+        memory[0] = memory.length;
+        memory[1] = NULL;
         freestart = 0;
     }
 
@@ -18,41 +18,40 @@ public class MemoryManager {
         int oldtop = top;
         top -= (requestSize+1);
         if (top<0) throw new StackOverflowError();
-        memeory[top] = oldtop;
+        memory[top] = oldtop;
         return top+1;
     }
 
     public void pop(){
-        top = memeory[top];
+        top = memory[top];
     }
 
     public int allocate(int requestSize){
         int size = requestSize+1;
         int p = freestart;
         int lag = NULL;
-        while(p != NULL && memeory[p]<size){
+        while(p != NULL && memory[p]<size){
             lag = p;
-            p = memeory[p+1];
+            p = memory[p+1];
         }
         if (p == NULL)
             throw new OutOfMemoryError();
-        int nextFree = memeory[p+1];
-        int unused = memeory[p]-size;
+        int nextFree = memory[p+1];
+        int unused = memory[p]-size;
         if (unused > 1){
             nextFree = p + size;
-            memeory[nextFree] = unused;
-            memeory[nextFree+1] = memeory[p+1];
-            memeory[p]= size;
+            memory[nextFree] = unused;
+            memory[nextFree+1] = memory[p+1];
+            memory[p]= size;
         }
         if (lag == NULL) freestart = nextFree;
-        else memeory[lag+1] = nextFree;
+        else memory[lag+1] = nextFree;
         return p+1;
     }
 
     public void deallocate(int address){
         int addr = address-1;
-        memeory[addr+1] = freestart;
+        memory[addr+1] = freestart;
         freestart = addr;
     }
-
 }
